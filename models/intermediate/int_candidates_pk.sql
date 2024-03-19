f03556149a4e922a7fce876ba73146758cb72e6e
Côme Laguarigue <come.laguarigue+lewagon@gmail.com>
2024-03-19 13:47:31 +0000

    Add timestamp

--- models/intermediate/int_candidates_pk.sql
+++ models/intermediate/int_candidates_pk.sql
@@ -1,5 +1,6 @@
 SELECT *,
- CONCAT (user_id," - ",shortlist_id," - ",status_update) as pk_candidates
+ CONCAT (user_id," - ",shortlist_id," - ",status_update) as pk_candidates,
+ TIMESTAMP_DIFF(timestamp(current_sign_in_at), timestamp(receive_time), HOUR) as timestamp_actions
 
  FROM {{ ref('int_candidates_deduplicates') }}
 
