74f008a23a254bd90c9d7ad5d208566b1164b82f
Côme Laguarigue <come.laguarigue+lewagon@gmail.com>
2024-03-20 11:49:51 +0000

    New table aggregated with average for country, school category and career center type

---
+++ models/mart/mart_students_agg_country_schoolcat_cctype.sql
@@ -0,0 +1,19 @@
+SELECT 
+  jt_country,
+  jt_school_category,
+  career_center_type,
+  round(COUNTIF(active) / COUNT(*) * 100,2) AS avg_active,
+  round(COUNTIF(resume_uploaded) / COUNT(*) *100,2) AS avg_resume_uploaded,
+  round(avg(nb_shortlists)*100,2) as avg_nb_shortlists,
+  round(avg(awaiting)*100,2) as avg_nb_awaiting,
+  round(avg(not_interested)*100,2) as avg_nb_not_interested,
+  round(avg(interested)*100,2) as avg_nb_interested,
+  round(avg(declined)*100,2) as avg_nb_declined,
+  round(avg(approved)*100,2) as avg_nb_approved
+
+FROM 
+  {{ ref('mart_students') }}
+GROUP BY 
+  jt_country,
+  jt_school_category,
+  career_center_type;
