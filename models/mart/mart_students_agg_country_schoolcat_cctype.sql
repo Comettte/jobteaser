63964b0bd323eed1340b01662bb217a6e0a12507
Côme Laguarigue <come.laguarigue+lewagon@gmail.com>
2024-03-21 14:38:07 +0000

    Modify bug window function table Agg_country_schoolcat_cctype

--- models/mart/mart_students_agg_country_schoolcat_cctype.sql
+++ models/mart/mart_students_agg_country_schoolcat_cctype.sql
@@ -1,16 +1,21 @@
-SELECT
+WITH aggregation AS 
+(SELECT
   school_id,
   jt_country,
   jt_school_category,
   career_center_type,
-  ROUND(COUNT(DISTINCT(user_id)) OVER (PARTITION BY jt_country, career_center_type, jt_school_category) / COUNT(DISTINCT(school_id)) OVER (PARTITION BY jt_country, career_center_type, jt_school_category), 0) AS avg_user_per_school,
-    ROUND(AVG(CASE WHEN active  = true THEN 1 ELSE 0 END) OVER (PARTITION BY jt_country, career_center_type, jt_school_category),2) AS avg_active,
-  ROUND(AVG(CASE WHEN resume_uploaded  = true THEN 1 ELSE 0 END) OVER (PARTITION BY jt_country, career_center_type, jt_school_category),2) AS avg_resume,
+  ROUND(COUNT(DISTINCT(user_id)) OVER (PARTITION BY jt_country, career_center_type, jt_school_category),2) AS avg_user_per_school,
+  ROUND(SUM(active) OVER (PARTITION BY jt_country, career_center_type, jt_school_category),2) AS avg_active,
+  ROUND(SUM(resume_uploaded) OVER (PARTITION BY jt_country, career_center_type, jt_school_category),2) AS avg_resume,
+  ROUND(SUM(nb_shortlists) OVER (PARTITION BY jt_country, career_center_type, jt_school_category), 2) AS avg_nb_shortlists,
+  ROUND(SUM(awaiting) OVER (PARTITION BY jt_country, career_center_type, jt_school_category), 2) AS avg_nb_awaiting,
+  ROUND(SUM(not_interested) OVER (PARTITION BY jt_country, career_center_type, jt_school_category), 2) AS avg_nb_not_interested,
+  ROUND(SUM(interested) OVER (PARTITION BY jt_country, career_center_type, jt_school_category), 2) AS avg_nb_interested,
+  ROUND(SUM(declined) OVER (PARTITION BY jt_country, career_center_type, jt_school_category), 2) AS avg_nb_declined
 
-  ROUND(AVG(nb_shortlists) OVER (PARTITION BY jt_country, career_center_type, jt_school_category), 2) AS avg_nb_shortlists,
-  ROUND(AVG(awaiting) OVER (PARTITION BY jt_country, career_center_type, jt_school_category), 2) AS avg_nb_awaiting,
-  ROUND(AVG(not_interested) OVER (PARTITION BY jt_country, career_center_type, jt_school_category), 2) AS avg_nb_not_interested,
-  ROUND(AVG(interested) OVER (PARTITION BY jt_country, career_center_type, jt_school_category), 2) AS avg_nb_interested,
-  ROUND(AVG(declined) OVER (PARTITION BY jt_country, career_center_type, jt_school_category), 2) AS avg_nb_declined
 FROM 
-  {{ ref('mart_students') }}
+  {{ ref('mart_students') }})
+
+SELECT DISTINCT *
+
+FROM aggregation
