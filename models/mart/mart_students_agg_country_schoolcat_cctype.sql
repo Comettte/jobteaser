864e24ea2d0e4e11f8d5af70953e1594ddd0aaa1
Lise Soskin <lise.skn@gmail.com>
2024-03-21 10:25:38 +0000

    Aggregation (partition by) -> analyse avg/secteurs

--- models/mart/mart_students_agg_country_schoolcat_cctype.sql
+++ models/mart/mart_students_agg_country_schoolcat_cctype.sql
@@ -1,20 +1,15 @@
-SELECT 
+SELECT
+  school_id,
   jt_country,
   jt_school_category,
   career_center_type,
-  ROUND(COUNT(DISTINCT(user_id))/COUNT(DISTINCT(school_id)),2) AS avg_user_per_schools,
-  round(COUNTIF(active) / COUNT(*) * 100,2) AS avg_active,
-  round(COUNTIF(resume_uploaded) / COUNT(*) *100,2) AS avg_resume_uploaded,
-  round(avg(nb_shortlists)*100,2) as avg_nb_shortlists,
-  round(avg(awaiting)*100,2) as avg_nb_awaiting,
-  round(avg(not_interested)*100,2) as avg_nb_not_interested,
-  round(avg(interested)*100,2) as avg_nb_interested,
-  round(avg(declined)*100,2) as avg_nb_declined,
-  round(avg(approved)*100,2) as avg_nb_approved
-
+  ROUND(COUNT(DISTINCT(user_id)) OVER (PARTITION BY jt_country, career_center_type, jt_school_category) / COUNT(DISTINCT(school_id)) OVER (PARTITION BY jt_country, career_center_type, jt_school_category), 0) AS avg_user_per_school,
+  ROUND(COUNTIF(active) OVER (PARTITION BY jt_country, career_center_type, jt_school_category) / COUNT(*) OVER (PARTITION BY jt_country, career_center_type, jt_school_category) * 100, 2) AS percentage_active_students,
+  ROUND(COUNTIF(resume_uploaded)OVER (PARTITION BY jt_country, career_center_type, jt_school_category) / COUNT(*) OVER (PARTITION BY jt_country, career_center_type, jt_school_category) * 100, 2) AS percentage_resume_uploaded,
+  ROUND(AVG(nb_shortlists) OVER (PARTITION BY jt_country, career_center_type, jt_school_category), 2) AS avg_nb_shortlists,
+  ROUND(AVG(awaiting) OVER (PARTITION BY jt_country, career_center_type, jt_school_category), 2) AS avg_nb_awaiting,
+  ROUND(AVG(not_interested) OVER (PARTITION BY jt_country, career_center_type, jt_school_category), 2) AS avg_nb_not_interested,
+  ROUND(AVG(interested) OVER (PARTITION BY jt_country, career_center_type, jt_school_category), 2) AS avg_nb_interested,
+  ROUND(AVG(declined) OVER (PARTITION BY jt_country, career_center_type, jt_school_category), 2) AS avg_nb_declined
 FROM 
   {{ ref('mart_students') }}
-GROUP BY 
-  jt_country,
-  jt_school_category,
-  career_center_type
