d56511305bd526a525aab930ea1729964a46236a
Côme Laguarigue <come.laguarigue+lewagon@gmail.com>
2024-03-21 08:46:11 +0000

    Fusion int_candidates_pk & deduplicates, suppression deduplicates

--- models/intermediate/int_candidates_pk.sql
+++ models/intermediate/int_candidates_pk.sql
@@ -1,6 +1,11 @@
+WITH deduplicates AS
+(SELECT DISTINCT *
+FROM {{ ref('stg_raw__candidate_status_update') }})
+
 SELECT *,
+
  CONCAT (user_id," - ",shortlist_id," - ",status_update) as pk_candidates,
  TIMESTAMP_DIFF(timestamp(current_sign_in_at), timestamp(receive_time), HOUR) as timestamp_actions
 
- FROM {{ ref('int_candidates_deduplicates') }}
+ FROM deduplicates
 
